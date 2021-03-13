a59d4fa7419311287bcb6325cc56ffbd794a6f53
Chrisi Webster <christinawebster.cw@gmail.com>
2021-03-13 12:51:29 +0000

    Started adding the weather API

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,23 @@
 const menuButton = document.querySelector(".menu");
-const navMenu = document.querySelector(".mobile-nav-wrapper");
+const navMenu = document.querySelector(".mobile-nav-links");
 const h1 = document.querySelector("h1");
 const daysCountdown = document.querySelector("#days");
 const hoursCountdown = document.querySelector("#hours");
 const minsCountdown = document.querySelector("#mins");
 const secsCountdown = document.querySelector("#secs");
 const weddingDate = new Date("July 31, 2021 14:30").getTime();
+const weatherDisplay = document.querySelector("#weather-display");
 
-menuButton.addEventListener("click", function () {
-  menuButton.classList.toggle("opened");
-  navMenu.classList.toggle("toggled");
-  h1.classList.toggle("z-toggle"); //menu was appearing behind the heading because the heading has zindex 1.
-});
+if (menuButton.style.display == "none") {
+  //Added this if statement because the js wasn't loading because it said I was trying to add an event listener to something that didn't exist.
+  menuButton.addEventListener("click", function () {
+    menuButton.classList.toggle("opened");
+    navMenu.classList.toggle("toggled");
+    h1.classList.toggle("z-toggle"); //menu was appearing behind the heading because the heading has z-index 1.
+  });
+}
 
+//Countdown clock
 const x = setInterval(() => {
   let now = new Date().getTime();
   /*Sees how long is left*/
@@ -32,3 +37,28 @@ const x = setInterval(() => {
     clearInterval(x);
   }
 }, 1000);
+
+//Weather API
+import key from "./_apikey.js";
+fetch(`https://api.openweathermap.org/data/2.5/weather?id=3034125&appid=${key}`)
+  .then(
+    (response) => {
+      if (response.ok) {
+        return response.json();
+      } else if (response.status_code == 404) {
+        console.log("Oh no. They could not find what you were looking for");
+      } else {
+        throw new Error("Request failed!");
+      }
+    },
+    (networkError) => {
+      console.log(networkError.message);
+    }
+  )
+  .then((jsonResponse) => {
+    // console.log(jsonResponse["weather"][0]);
+    switch (true) {
+      case jsonResponse["weather"][0]["main"] == "Rain":
+        weatherDisplay.innerHTML = '<i class="fas fa-cloud-rain"></i>';
+    }
+  });
