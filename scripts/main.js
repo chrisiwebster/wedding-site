e28cf34d9bcfe866155d79e3e0f4b815687a9e10
Chrisi Webster <christinawebster.cw@gmail.com>
2021-04-10 17:20:05 +0100

    remove if statement around menuButton

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,7 @@
 const menuButton = document.querySelector(".menu");
 const navLinks = document.querySelector(".nav-links");
 
-//Added this statement because the js wasn't loading because it said I was trying to add an event listener to something that didn't exist.
-if (menuButton) {
-  menuButton.addEventListener("click", () => {
-    menuButton.classList.toggle("opened");
-    navLinks.classList.toggle("toggled");
-  });
-}
+menuButton.addEventListener("click", () => {
+  menuButton.classList.toggle("opened");
+  navLinks.classList.toggle("toggled");
+});
